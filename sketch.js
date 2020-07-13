173379b155b32be07d97ca0f276792d65b1770f6
AamilKalra <63241401+AamilKalra@users.noreply.github.com>
2020-07-13 17:04:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,13 +17,13 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 	ground = new Ground(600,590,1200,20);
-	paperball = new Paper(1100,200,70);
+	paperball = new Paper(100,200,20);
 	//packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
 	//World.add(world, packageBody);
 
-	side1= createSprite(800,500,20,170)
-	side2= createSprite(950,500,20,170)
-	side3= createSprite(875,570,170,20)
+	side1 = new Dustbin(800,500,20,162)
+	side2 = new Dustbin(950,500,20,162)
+	side3 = new Dustbin(875,570,170,20)
 	
 	//Create a Ground
 	//ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
@@ -42,14 +42,22 @@ function draw() {
   
   ground.display();
   paperball.display();
+  side1.display();
+  side2.display();
+  side3.display();
   drawSprites();
 }
 
 function keyPressed(){
-	if (keyCode === UP_ARROW){
-		Matter.Body.applyForce(paperball.body,paperball.body.position,{x:90,y:-100});
+	if (keyCode === UP_ARROW ){
+		Matter.Body.applyForce(paperball.body,paperball.body.position,{x:70,y:-80});
+		console.log(paperball.body.position)
 	}
+	
 }
+
+
+
 
 
 
